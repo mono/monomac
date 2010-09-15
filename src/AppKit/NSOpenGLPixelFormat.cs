88547b4b78e9a10fea3be6c47e9541772b765a3c
Joe Mattiello <joe@sooloos.com>
2010-09-15 03:31:20 -0400

    Added OpenGL wrapper functions to deal with passing of C pointer args

---
+++ src/AppKit/NSOpenGLPixelFormat.cs
@@ -0,0 +1,62 @@
+//
+// Copyright 2010, Novell, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+using System;
+
+using System.Drawing;
+
+using System.Runtime.InteropServices;
+
+using MonoMac.CoreFoundation;
+
+using MonoMac.Foundation;
+
+using MonoMac.ObjCRuntime;
+
+using MonoMac.CoreGraphics;
+
+using MonoMac.CoreAnimation;
+
+namespace MonoMac.AppKit {
+    partial class NSOpenGLPixelFormat {
+        static IntPtr selInitWithAttributes = Selector.GetHandle ("initWithAttributes:");
+		
+        [Export ("initWithAttributes:")]
+		public NSOpenGLPixelFormat (NSOpenGLPixelFormatAttribute[] attribs) : base (NSObjectFlag.Empty)
+		{
+			if (attribs == null)
+				throw new ArgumentNullException ("attribs");
+        
+            unsafe {
+                fixed (NSOpenGLPixelFormatAttribute* pArray = attribs){
+			        if (IsDirectBinding) {
+				        Handle = MonoMac.ObjCRuntime.Messaging.IntPtr_objc_msgSend_IntPtr (this.Handle, selInitWithAttributes, new IntPtr((void*)pArray ));
+			        } else {
+				        Handle = MonoMac.ObjCRuntime.Messaging.IntPtr_objc_msgSendSuper_IntPtr (this.SuperHandle, selInitWithAttributes, new IntPtr((void*)pArray));
+			        }
+		        }
+            }
+		}
+		
+
+    }
+}
