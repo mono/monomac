b744cf08c691a7a0216162f42044bb5924350b33
Geoff Norton <grompf@sublimeintervention.com>
2010-10-14 20:06:38 -0400

    Fix QTKit implicit loading

--- src/Foundation/NSObjectMac.cs
+++ src/Foundation/NSObjectMac.cs
@@ -34,7 +34,7 @@ namespace MonoMac.Foundation {
 		static IntPtr fl = Dlfcn.dlopen (Constants.FoundationLibrary, 1);
 		static IntPtr al = Dlfcn.dlopen (Constants.AppKitLibrary, 1);
 		static IntPtr wl = Dlfcn.dlopen (Constants.WebKitLibrary, 1);
-			
+		static IntPtr ql = Dlfcn.dlopen (Constants.QTKitLibrary, 1);
 	}
 }
 
