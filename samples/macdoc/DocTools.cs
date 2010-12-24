67d0c684b2ff5c68744943bce8b6c00301793913
Miguel de Icaza <miguel@gnome.org>
2010-12-23 21:39:23 -0500

    Add missing file

---
+++ samples/macdoc/DocTools.cs
@@ -0,0 +1,45 @@
+//
+// A few routines that were copy-pasted from the docbrowser
+//
+
+using System;
+using Monodoc;
+using System.IO;
+
+namespace macdoc
+{
+	public class DocTools {
+		public static string GetHtml (string url, HelpSource helpSource)
+		{
+			string htmlContent = null;
+			Node match;
+			
+			if (helpSource != null)
+				htmlContent = helpSource.GetText (url, out match);
+			if (htmlContent == null){
+				htmlContent = AppDelegate.Root.RenderUrl (url, out match);
+				if (htmlContent != null && match != null && match.tree != null){
+					helpSource = match.tree.HelpSource;
+				}
+			}
+			if (htmlContent == null)
+				return null;
+
+			var html = new StringWriter ();
+   			html.Write ("<html>\n<head><title>{0}</title>", url);
+			
+			if (helpSource != null){
+            	if (helpSource.InlineCss != null) 
+                    html.Write (" <style type=\"text/css\">{0}</style>\n", helpSource.InlineCss);
+				if (helpSource.InlineJavaScript != null)
+                    html.Write ("<script type=\"text/JavaScript\">{0}</script>\n", helpSource.InlineJavaScript);
+            }
+
+            html.Write ("</head><body>");
+            html.Write (htmlContent);
+            html.Write ("</body></html>\n");
+            return html.ToString ();
+		}
+	}
+}
+
