ec7aee99dba07d6c5b425f06e24f9b2dfbd4b06f
Duane Wandless <dwandless@pixmaclt10.local>
2011-02-17 11:29:41 -0500

    fix whitespace and remove for loop

--- src/ObjCRuntime/Runtime.cs
+++ src/ObjCRuntime/Runtime.cs
@@ -39,28 +39,26 @@ namespace MonoMac.ObjCRuntime {
 		
 		public static void RegisterAssembly (Assembly a) {
 			var attributes = a.GetCustomAttributes (typeof (RequiredFrameworkAttribute), false);
-	                string basePath = Path.Combine (AppDomain.CurrentDomain.BaseDirectory, "..");
+			string basePath = Path.Combine (AppDomain.CurrentDomain.BaseDirectory, "..");
 
-			for (int i = 0; i < max; i++) {
-				foreach (var attribute in attributes) {
-					var requiredFramework = (RequiredFrameworkAttribute)attribute;
-					string libPath;
-					string libName = requiredFramework.Name;
-					
-					if (libName.Contains (".dylib")) {
-						libPath = Path.Combine (basePath, "Resources");
-					}
-					else {
-						libPath = Path.Combine (basePath, "Frameworks");
-						libPath = Path.Combine (libPath, libName);
-						libName = libName.Replace (".frameworks", "");
-					}
-					libPath = Path.Combine (libPath, libName);
-					
-					if (Dlfcn.dlopen (libPath, 0) == IntPtr.Zero)
-						throw new Exception (string.Format ("Unable to load required framework: '{0}'", requiredFramework.Name),
-					new Exception (Dlfcn.dlerror()));
+			foreach (var attribute in attributes) {
+				var requiredFramework = (RequiredFrameworkAttribute)attribute;
+				string libPath;
+				string libName = requiredFramework.Name;
+				
+				if (libName.Contains (".dylib")) {
+					libPath = Path.Combine (basePath, "Resources");
 				}
+				else {
+					libPath = Path.Combine (basePath, "Frameworks");
+					libPath = Path.Combine (libPath, libName);
+					libName = libName.Replace (".frameworks", "");
+				}
+				libPath = Path.Combine (libPath, libName);
+				
+				if (Dlfcn.dlopen (libPath, 0) == IntPtr.Zero)
+					throw new Exception (string.Format ("Unable to load required framework: '{0}'", requiredFramework.Name),
+				new Exception (Dlfcn.dlerror()));
 			}
 			
 			if (assemblies == null) {
