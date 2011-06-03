7ed20cec90c81b81438933a96c6ac6722c56000b
Frank A. Krueger <fak@praeclarum.org>
2011-06-02 19:08:02 -0700

    Modified ConvertArguments to not call GetNSObject for parameters marked with OutAttribute

--- src/ObjCRuntime/NativeImplementationBuilder.cs
+++ src/ObjCRuntime/NativeImplementationBuilder.cs
@@ -190,7 +190,7 @@ namespace MonoMac.ObjCRuntime {
 		protected void ConvertArguments (ILGenerator il, int locoffset) {
 #if !MONOMAC_BOOTSTRAP
 			for (int i = ArgumentOffset, j = 0; i < ParameterTypes.Length; i++) {
-				if (Parameters [i-ArgumentOffset].ParameterType.IsByRef && IsWrappedType (Parameters[i-ArgumentOffset].ParameterType.GetElementType ())) {
+				if (Parameters [i-ArgumentOffset].ParameterType.IsByRef && (Attribute.GetCustomAttribute (Parameters [i-ArgumentOffset], typeof (OutAttribute)) == null) && IsWrappedType (Parameters[i-ArgumentOffset].ParameterType.GetElementType ())) {
 					il.Emit (OpCodes.Ldarg, i);
 					il.Emit (OpCodes.Ldind_I);
 					il.Emit (OpCodes.Call, getobject);
