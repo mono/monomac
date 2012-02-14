7c9ce65d34712c20efa53752185218dac330a8d0
Jeremie Laval <jeremie.laval@gmail.com>
2012-02-14 20:27:01 +0100

    Fix naming in NSPopover binding

--- src/appkit.cs
+++ src/appkit.cs
@@ -7778,7 +7778,7 @@ namespace MonoMac.AppKit {
 		NSObject WeakDelegate { get; set; }
 
 		[Wrap ("WeakDelegate")]
-		NSPopoverDelegate Delegat { set; get; }
+		NSPopoverDelegate Delegate { set; get; }
 		
 		[Export ("showRelativeToRect:ofView:preferredEdge:")]
 		void Show (RectangleF relativePositioningRect, NSView positioningView, NSRectEdge preferredEdge);
