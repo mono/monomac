4e010a60aa9502dbce0dd5f2d4548bcdb4bed421
Richard Markiewicz <richardjm.mail@gmail.com>
2013-09-11 09:40:29 -0400

    Fix return type of NSTreeNode representedObject - should be NSObject, not NSTreeNode

--- src/appkit.cs
+++ src/appkit.cs
@@ -13844,7 +13844,7 @@ namespace MonoMac.AppKit {
 		IntPtr Constructor (NSObject modelObject);
 
 		[Export ("representedObject")]
-		NSTreeNode RepresentedObject { get; }
+		NSObject RepresentedObject { get; }
 
 		[Export ("indexPath")]
 		NSIndexPath IndexPath { get; }
