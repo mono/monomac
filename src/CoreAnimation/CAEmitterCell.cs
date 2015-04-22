a88a1700443f334e6eba73d71b73ddb37f63bc71
Curtis Wensley <curtis.wensley@gmail.com>
2015-04-21 19:28:46 -0700

    Add CAEmitterCell.Content for Fire sample

---
+++ src/CoreAnimation/CAEmitterCell.cs
@@ -0,0 +1,87 @@
+//
+// CAEmitterCell.cs
+//
+// Author:
+//   Curtis Wensley <curtis.wensley@gmail.com>
+//
+// Copyright 2012 Xamarin Inc. (http://xamarin.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Collections.Generic;
+
+using MonoMac.Foundation;
+
+#if MAC64
+using nint = System.Int64;
+using nuint = System.UInt64;
+using nfloat = System.Double;
+#else
+using nint = System.Int32;
+using nuint = System.UInt32;
+using nfloat = System.Single;
+#if SDCOMPAT
+using CGPoint = System.Drawing.PointF;
+using CGSize = System.Drawing.SizeF;
+using CGRect = System.Drawing.RectangleF;
+#endif
+#endif
+
+using System;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using MonoMac;
+using MonoMac.CoreFoundation;
+using MonoMac.Foundation;
+using MonoMac.ObjCRuntime;
+using MonoMac.CoreGraphics;
+using MonoMac.CoreAnimation;
+using MonoMac.CoreLocation;
+using MonoMac.QTKit;
+using MonoMac.CoreVideo;
+using MonoMac.CoreMedia;
+using MonoMac.OpenGL;
+
+namespace MonoMac.CoreAnimation {
+	
+	partial class CAEmitterCell {
+		CGImage contents;
+	
+		public CGImage Contents
+		{
+			get {
+				if (contents != null)
+					return contents;
+				var ptr = _Contents;
+				return ptr != IntPtr.Zero ? (contents = new CGImage(ptr)) : null; 
+			}
+			set { 
+				_Contents = value != null ? value.Handle : IntPtr.Zero;
+				contents = value;
+			}
+		}
+	} 
+}
